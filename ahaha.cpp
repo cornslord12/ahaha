df07a1e2fe048259482a856c78a2a0e3dd38d06c
steelry <nastena.trofimenko@outlook.com>
2025-04-03 19:33:10 +0300

    1.1

--- ahaha.cpp
+++ ahaha.cpp
@@ -9,5 +9,12 @@ int main()
    cout << "Come here.\n";
    cout << "And feel safe!\n";
    cout << ":)";
+   cout << "!";
+
+   cout << "nah\n";
+   cout << "do not!\n";
+
+   cout << "nooooo :(\n";
+   cout << "I've changed my mi-i-ind!!!\n";
 }
 
